c3d3ca8221f21c07ff8a9da4f452c5c1e31474b6
Hugo Guimarães-ESTG-IPP <148533705+Hugo8220337@users.noreply.github.com>
2025-07-28 12:30:35 +0100

    #add: Cartridge get methods
    Added:
    - getTitle
    - getCartridgeType
    - getRomSize
    - getRomBanks
    -getRAMSize
    - getRAMBanks

--- app/src/main/java/gb_emu/App.java
+++ app/src/main/java/gb_emu/App.java
@@ -1,31 +1,11 @@
 package gb_emu;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import gb_emu.core.mem.cartridge.Cartridge;
 
 public class App {
     public static void main(String[] args) {
-        String filePath = "/home/hugo-guimar-es/Documents/Tetris (World).gb";
-        File file = new File(filePath);
-        byte[] romData = new byte[(int) file.length()];
-
-        try (FileInputStream fis = new FileInputStream(file)) {
-            int bytesRead = fis.read(romData);
-            if(bytesRead != romData.length) {
-                throw new IOException("Error while reading the ROM file.");
-            }
-        } catch (Exception e) {
-            System.out.println("Unexpected Error!");
-        }
-
-        // printa como deve de ser os bytes
-        for (int i = 0; i < romData.length; i++) {
-            int unsigned = romData[i] & 0xFF; // tira o sinal
-            System.out.print(unsigned); // mo
-            if ((i + 1) % 16 == 0) System.out.println(); // Quebra de linha a cada 16 bytes
-        }
-        
-        // return romData;
+        String filePath = "/home/hugo-guimar-es/roms/gb/Tetris (World).gb";
+        Cartridge cartridge = new Cartridge(filePath);
+        System.out.println(cartridge.getCartridgeType());
     }
 }
--- app/src/main/java/gb_emu/core/mem/cartridge/Cartridge.java
+++ app/src/main/java/gb_emu/core/mem/cartridge/Cartridge.java
@@ -1,28 +1,160 @@
 package gb_emu.core.mem.cartridge;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
 
-public class Cartridge {
-    private  static final Logger logger = LoggerFactory.getLogger(Cartridge.class);
-    
+public class Cartridge implements Serializable{
     private byte[] romData;
 
-    public void loadFile(String path) throws IOException {
+    public Cartridge(String romPath) {
+        this.romData = loadFile(romPath);
+    }
+
+    /**
+     * Read the rom in the specified path
+     * 
+     * @param path
+     * @return byte array with the rom data
+     * @throws RuntimeException
+     */
+    private byte[] loadFile(String path) throws RuntimeException {
         File file = new File(path);
-        this.romData = new byte[(int) file.length()];
-        
+        byte[] data = new byte[(int) file.length()];
+
         try (FileInputStream fis = new FileInputStream(file)) {
-            int bytesRead = fis.read(romData);
-            if(bytesRead != romData.length) {
+            int bytesRead = fis.read(data);
+            if (bytesRead != data.length) {
                 throw new IOException("Error while reading the ROM file.");
             }
-        } catch(Exception e) {
-            throw new IOException("Error while reading the ROM file.");
+        } catch (Exception e) {
+            throw new RuntimeException("Error while reading the ROM file.");
+        }
+
+        return data;
+    }
+
+    public String getTitle() {
+        byte[] titleBytes = Arrays.copyOfRange(romData, 0x0134, 0x0143);
+        return new String(titleBytes).trim();
+    }
+
+    public CartridgeType getCartridgeType() {
+        int cartridgeTypeCode = Byte.toUnsignedInt(romData[0x0147]);
+        return CartridgeType.getFromCode(cartridgeTypeCode);
+    }
+
+    /**
+     * @return number of rom banks in BiB
+     */
+    public int getROMSize() {
+        int romSizeCode = Byte.toUnsignedInt(romData[0x0148]);
+        switch (romSizeCode) {
+            case 0x00:
+                return 32; // 32 KiB
+            case 0x01:
+                return 64; // 64 KiB
+            case 0x02:
+                return 128; // 128 KiB
+            case 0x03:
+                return 256; // 256 KiB
+            case 0x04:
+                return 512; // 512 KiB
+            case 0x05:
+                return 1024; // 1 MiB
+            case 0x06:
+                return 2048; // 2 MiB
+            case 0x07:
+                return 4096; // 4 MiB
+            case 0x08:
+                return 8192; // 8 MiB
+            case 0x52:
+                return 1126; // 1.1 MiB (aprox. 1126 KiB)
+            case 0x53:
+                return 1280; // 1.2 MiB (aprox. 1280 KiB)
+            case 0x54:
+                return 1536; // 1.5 MiB (aprox. 1536 KiB)
+            default:
+                throw new IllegalArgumentException("Illegal ROM size code: " + romSizeCode);
+        }
+    }
+
+    public int getROMBanks() {
+        int romSizeCode = Byte.toUnsignedInt(romData[0x0148]);
+        switch (romSizeCode) {
+            case 0x00:
+                return 2; // 2 banks (no banking)
+            case 0x01:
+                return 4;
+            case 0x02:
+                return 8;
+            case 0x03:
+                return 16;
+            case 0x04:
+                return 32;
+            case 0x05:
+                return 64;
+            case 0x06:
+                return 128;
+            case 0x07:
+                return 256;
+            case 0x08:
+                return 512;
+            case 0x52:
+                return 72;
+            case 0x53:
+                return 80;
+            case 0x54:
+                return 96;
+            default:
+                throw new IllegalArgumentException("Illegal ROM size code: " + romSizeCode);
+        }
+    }
+
+    public int getRAMSize() {
+        int ramSizeCode = Byte.toUnsignedInt(romData[0x0149]);
+        switch (ramSizeCode) {
+            case 0x00:
+                return 0;
+            case 0x01:
+                return 0; // Unused
+            case 0x02:
+                return 8;
+            case 0x03:
+                return 32;
+            case 0x04:
+                return 128;
+            case 0x05:
+                return 64;
+            default:
+                throw new IllegalArgumentException("Illegal ram size, received " + ramSizeCode + " code");
+        }
+    }
+
+    /**
+     * Get the number of ram banks (each ram bank has 8KiB)
+     * 
+     * @return number of ram banks
+     */
+    public int getRAMBanks() {
+        int ramSizeCode = Byte.toUnsignedInt(romData[0x0149]);
+        switch (ramSizeCode) {
+            case 0x00:
+                return 0;
+            case 0x01:
+                return 0; // Unused
+            case 0x02:
+                return 1;
+            case 0x03:
+                return 4;
+            case 0x04:
+                return 16;
+            case 0x05:
+                return 8;
+            default:
+                throw new IllegalArgumentException("Illegal ram size, received " + ramSizeCode + " code");
         }
     }
 }
