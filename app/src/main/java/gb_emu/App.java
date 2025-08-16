a4cd4871404ad8ab5fc382e464e535c6adcc4d76
Hugo Guimarães-ESTG-IPP <148533705+Hugo8220337@users.noreply.github.com>
2025-08-16 11:27:56 +0100

    Update App.java

--- app/src/main/java/gb_emu/App.java
+++ app/src/main/java/gb_emu/App.java
@@ -3,21 +3,27 @@ package gb_emu;
 import gb_emu.core.GameBoy;
 import gb_emu.core.mem.cartridge.Cartridge;
 import gb_emu.ui.Screen;
+import gb_emu.ui.debugScreen.DebugScreen;
 
 public class App {
     public static void main(String[] args) {
-        String filePath = "/home/hugo-guimar-es/roms/gb/Tetris (World).gb";
-        String botRomPath ="/home/hugo-guimar-es/roms/gb/bootix_dmg.bin";
+        String filePath = "/home/hugo-guimar-es/roms/GB/Dr. Mario.gb";
+        // String filePath =
+        // "/home/hugo-guimar-es/roms/GB/test_roms/blargg/cpu_instrs.gb";
+        String botRomPath = "/home/hugo-guimar-es/roms/GB/bootix_dmg.bin";
         Cartridge cartridge = new Cartridge(filePath, botRomPath);
         System.out.println(cartridge.getCartridgeType());
 
         GameBoy gameBoy = new GameBoy(cartridge);
-
-        new Thread(() -> {
-            gameBoy.start();
-        }).start();
-
-        Screen screen = new Screen(160, 144, gameBoy);
+        
+        
+        DebugScreen debugScreen = new DebugScreen(gameBoy);
+        debugScreen.setVisible(true);
+        
+        Screen screen = new Screen(160, 144);
+        gameBoy.setObsever(screen);
         screen.start();
+        
+        gameBoy.start();
     }
 }
